df3531cdbe19ec4fb4e7eb2d8bb9a0df4db62e9b
stefanwg <stefan.walzergoldfeld@gmail.com>
2020-04-06 19:01:10 -0400

    Possible Baseballfield

--- src/Game.java
+++ src/Game.java
@@ -3,11 +3,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.geom.Arc2D;
+import java.awt.image.BufferedImage;
 
 public class Game extends JPanel {
     public Player player;
     final int WIDTH = 1000;
     final int HEIGHT = 1000;
+    BufferedImage fieldDrawing = drawField();
 
     ContactMeter cm;
 
@@ -35,21 +37,24 @@ public class Game extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
-        drawField(g);
+        g.drawImage(fieldDrawing,0,0,null);
         cm.draw(g);
     }
 
-    public void drawField(Graphics gOri) {
-        int xOri = 400;
-        int yOri = 700;
-        Graphics2D g = (Graphics2D) gOri;
+    public BufferedImage drawField() {
+        int xOri = 250;
+        int yOri = 500;
+        //Graphics2D g = (Graphics2D) gOri;
+        BufferedImage image = new BufferedImage ( 500, 550, BufferedImage.TYPE_INT_ARGB );
+        final Graphics2D g = image.createGraphics ();
+
         float[] fracs = {.25f,.75f};
         Color[] colors = {new Color(0,102,0), new Color(0,179,0)};
-        LinearGradientPaint grass = new LinearGradientPaint(0,0, WIDTH,HEIGHT, fracs, colors, MultipleGradientPaint.CycleMethod.REPEAT);
+        LinearGradientPaint grass = new LinearGradientPaint(0,0, 800,800, fracs, colors, MultipleGradientPaint.CycleMethod.REPEAT);
         Color infield = new Color(153,77,0);
-        //Background -- Can be modified to be just around the field
+        //Background
         g.setColor(Color.lightGray);
-        g.fillRect(0,0,WIDTH,HEIGHT);
+        g.fillRect(0,0,800,800);
         //Warning Track
         g.setColor(infield);
         g.fill(new Arc2D.Float(xOri-230, yOri-190-230, 460, 460, 0, 360, Arc2D.PIE));
@@ -70,8 +75,8 @@ public class Game extends JPanel {
         drawRect(g, new Point(xOri, yOri-6), new Point(xOri+64-6, yOri-64), new Point(xOri, yOri-128+6), new Point(xOri-64+6, yOri-64));
         //Clear sides
         g.setColor(Color.lightGray);
-        int[] tXL = {xOri, xOri-255-10, xOri-255, xOri+255, xOri+255+10};
-        int[] tYL = {yOri+10, yOri-255, yOri+50,yOri+50, yOri-255};
+        int[] tXL = {xOri, xOri-225-10, xOri-225, xOri+225, xOri+225+10};
+        int[] tYL = {yOri+10, yOri-225, yOri+50,yOri+50, yOri-225};
         g.fillPolygon(tXL,tYL,5);
         //Pitchers Mound and base areas
         g.setColor(infield);
@@ -89,11 +94,15 @@ public class Game extends JPanel {
         drawRect(g, new Point(xOri+64, yOri-64), new Point(xOri+64-3, yOri-64-3), new Point(xOri+64-6, yOri-64), new Point(xOri+64-3, yOri-64+3));
         drawRect(g, new Point(xOri, yOri-128), new Point(xOri-3, yOri-128+3), new Point(xOri, yOri-128+6), new Point(xOri+3, yOri-128+3));
         drawRect(g, new Point(xOri-64, yOri-64), new Point(xOri-64+3, yOri-64+3), new Point(xOri-64+6, yOri-64), new Point(xOri-64+3, yOri-64-3));
-        g.fillRect(xOri-2,yOri-4,4,2);
+        g.fillRect(xOri-2,yOri-4,5,2);
         int[] tXR = {xOri, xOri+2, xOri-2};
         int[] tYR = {yOri, yOri-2, yOri-2};
         g.fillPolygon(tXR,tYR,3);
         g.fillRect(xOri-2,yOri-60-1,4,2);
+
+        image = makeTransparent(Color.lightGray, image);
+        return image;
+
     }
 
     public void drawRect(Graphics g, Point p1, Point p2, Point p3, Point p4) {
@@ -104,5 +113,24 @@ public class Game extends JPanel {
         int[] tXR = {p1.x, p2.x,  p3.x};
         int[] tYR = {p1.y, p2.y,  p3.y};
         g.fillPolygon(tXR,tYR,3);
+    }
+
+    public BufferedImage makeTransparent(Color c, BufferedImage image) {
+        BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), image.getType());
+        int transparent = new Color(0,0,0,0).getRGB();
+        int original = c.getRGB();
+
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                int current = image.getRGB(i,j);
+                if (current == original) {
+                    newImage.setRGB(i,j,transparent);
+                } else {
+                    newImage.setRGB(i,j, current);
+                }
+            }
+        }
+
+        return newImage;
     }
 }
